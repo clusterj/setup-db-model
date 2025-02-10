4a29ee5819ced181aad41a82f712eab8cc582109
Roberto Messa <wardog1@gmail.com>
2025-02-09 21:00:02 -0300

    feat: created id list by created

---
+++ procedure/session/session_id_list_by_account_and_created.sql
@@ -0,0 +1,18 @@
+USE clusterj_accounts;
+
+DELIMITER $
+
+DROP PROCEDURE IF EXISTS session_id_list_by_account_and_created $
+
+CREATE PROCEDURE session_id_list_by_account_and_created(
+    IN in_acct_id INTEGER
+)
+BEGIN
+
+    SELECT id id
+    FROM sess_session
+    WHERE (acct_id = in_acct_id) AND (started is NULL);
+
+END $
+
+DELIMITER ;
