a9f64b7331d6d73231b9878a65529266b4db5bbf
Roberto Messa <wardog1@gmail.com>
2025-02-19 22:18:18 -0300

    fix: changed status field

--- procedure/account/update_account_status.sql
+++ procedure/account/update_account_status.sql
@@ -15,7 +15,7 @@ CREATE PROCEDURE update_account_status(
 BEGIN
 
     UPDATE acct_account
-    SET account_status = in_status, updated = in_updated, statustoken = NULL
+    SET status = in_status, updated = in_updated, statustoken = NULL
     WHERE (id = in_id);
 
     SELECT ROW_COUNT() INTO out_row_count;
